a5fc090e1a59939336ba75e575c4e855cbd99cd9
Mathieu <degracia.mathieu@gmail.com>
2020-09-13 17:33:01 +0200

    Add simple jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,28 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float moveSpeed;
+    public float moveSpeed;
+	public float jumpForce;
+
+    public bool isJumping;
+    public bool isGrounded;
 
 	public Rigidbody2D rb;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
 
 	private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            isJumping = true;
+        }
 
         MovePlayer(horizontalMovement);
     }
@@ -20,5 +33,10 @@ public class PlayerMovement : MonoBehaviour
     	Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
 
     	rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping) {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
     }
 }
